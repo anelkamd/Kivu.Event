ab285708bc5ee84140d4206441637b1dc09247f9
Anelka MD <anelkabagalaneofficiel@gmail.com>
2025-01-02 09:27:01 +0100

    update

--- client/src/pages/Calendrier.jsx
+++ client/src/pages/Calendrier.jsx
@@ -4,7 +4,6 @@ const Calendrier = () => {
     return (
         <div className="p-6">
             <h1 className="text-2xl font-bold">Calendrier</h1>
-            {/* Contenu de la page Calendrier */}
         </div>
     );
 };
